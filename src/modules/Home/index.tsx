3dc840fd4a120404822442f70c56b36cc36b1b6a
pedrogilsenarego <pedrogilsenarego@gmail.com>
2022-12-01 15:56:10 +0000

    a

--- src/modules/Home/index.tsx
+++ src/modules/Home/index.tsx
@@ -3,17 +3,28 @@ import CardMedia from "../../components/CardMedia";
 import { projects } from "../../assets/content/projects";
 
 const Home = () => {
-  const Theme = useTheme()
-  const mobile = useMediaQuery(Theme.breakpoints.down("sm"))
-
+  const Theme = useTheme();
+  const mobile = useMediaQuery(Theme.breakpoints.down("sm"));
+  const extraLarge = useMediaQuery(Theme.breakpoints.up(2000));
 
   return (
     <>
-      <div style={{ justifyContent: "center", marginLeft: mobile ? "20px" : "80px", marginRight: mobile ? "20px" : "80px" }}>
-        <Grid container columnSpacing={mobile ? 3 : 10} rowSpacing={mobile ? 3 : 10} alignItems="center">
+      <div
+        style={{
+          justifyContent: "center",
+          marginLeft: mobile ? "20px" : "80px",
+          marginRight: mobile ? "20px" : "80px",
+        }}
+      >
+        <Grid
+          container
+          columnSpacing={mobile ? "20px" : "40px"}
+          rowSpacing={mobile ? "20px" : "40px"}
+          alignItems='center'
+        >
           {projects.map((image, pos) => {
             return (
-              <Grid item xs={6} md={4} xl={3}>
+              <Grid item xs={6} md={4} xl={extraLarge ? 2.4 : 3}>
                 <CardMedia image={image} key={pos} />
               </Grid>
             );
