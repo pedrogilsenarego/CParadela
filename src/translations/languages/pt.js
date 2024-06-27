2ff921343c021afac0d57ac4cfdb518016536a99
Pedro Rego <pedro.rego@stark.dk>
2024-06-27 09:10:01 +0100

    a

--- src/translations/languages/pt.js
+++ src/translations/languages/pt.js
@@ -37,11 +37,11 @@ export const content = {
 
         about: {
           mainText:
-            "Ricardo Conde Sousa was born in 1988 in Cascais, Portugal. He studied architecture at Universidade Lusíada de Lisboa and Accademia di Architettura di Mendrisio, where he was a student of Professor Valerio Olgiati and graduated under Professor Jonathan Sergison in 2012. Before establishing his own practice in 2017, Ricardo worked as an architect at Weberbrunner Architekten in Zürich, SCML in Lisbon and Atelier Peter Zumthor in Haldenstein. From 2016 until 2019 he was a teaching assistant at Studio Canevascini, under the Chair of Professor Mario Botta, at Accademia di Architettura di Mendrisio.",
+            "Ricardo Conde de Sousa nasceu em 1988 em Cascais. Estudou arquitectura na Universidade Lusíada de Lisboa e na Accademia di Architettura di Mendrisio, onde foi aluno do Professor Valerio Olgiati e onde se diplomou com o o Professor Jonathan Sergison, em 2012. Antes de estabelecer a sua própria prática, Ricardo trabalhou como arquitecto nos Weberbrunner Architekten em Zurique, na SCML em Lisboa e no Atelier Peter Zumthor em Haldenstein, Suíça. Entre 2016 e 2019 foi assistente do atelier Canevascini, sob a coordenação do Professor Mario Botta, na Accademia di Architettura di Mendrisio.",
           second:
             "Sofia Paradela was born in 1989 in Lisbon, Portugal. She studied Product Design at Faculdade de Belas-Artes, in Lisbon, and Nuova Accademia di Belle Arti, in Milan. Later, she studied architecture, in Faculdade de Arquitectura da Universidade Técnica de Lisboa and Accademia di Architettura di Mendrisio. Here, she learned from Professor Valerio Olgiati and graduated in 2016 under Professors Francisco and Manuel Aires Mateus. Sofia worked as an intern at Aires Mateus e Associados in Lisbon and as an architect at Capaul&Blumenthal in Ilanz and at Atelier Peter Zumthor in Haldenstein. From 2020 she has been a lecturer at Accademia di Architettura di Mendrisio, leading her own first year Bachelor´s design studio, under the Chair of professor Valerio Olgiati.",
           third:
-            "They are currently based in Lisbon where they founded their architecture practice in 2019.",
+            "Fundaram o seu próprio atelier de arquitectura em 2019, em Lisboa, onde vivem e trabalham actualmente.",
           works: "Projectos",
           email: "info@condeparadela.com",
           address: "praça joão do rio 8, rc dto.\n 1000-180 lisboa\n Portugal",
